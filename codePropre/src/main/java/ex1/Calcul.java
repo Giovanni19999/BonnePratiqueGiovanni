87a6f6d5916835665220e84462f919328e34ab45
Giovanni BATY <giogio85.gb@gmail.com>
2023-03-27 15:53:16 +0200

    feat!: TP02 ex1
    Author: Giovanni

---
+++ codePropre/src/main/java/ex1/Calcul.java
@@ -0,0 +1,25 @@
+package ex1;
+
+public class Calcul {
+
+	/**
+	 * ma doc
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+
+
+	/**
+	 * ma doc
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
