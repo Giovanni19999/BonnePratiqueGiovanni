2f759c856fa8975a8442cb971d1f52702b9b26e5
Giovanni BATY <giogio85.gb@gmail.com>
2023-03-29 21:43:09 +0200

    feat: TP01

--- codePropre/src/main/java/ex2/LivretA.java
+++ codePropre/src/main/java/ex2/LivretA.java
@@ -7,9 +7,7 @@ public class LivretA extends CompteBancaire {
 	private double tauxRemuneration;
 
 	/** Ce constructeur est utilisé pour créer un compte de type Livret A
-	 * @param type = LA
 	 * @param solde représente le solde du compte
-	 * @param decouvert  représente le découvert autorisé
 	 * @param tauxRemuneration  représente le taux de rémunération du livret A
 	 */
 	public LivretA(double solde,  double tauxRemuneration) {
